c94f188df78ad5472b23344366732f3bef94dc6a
Prakhar Gupta <prakhargupta3510@gmail.com>
2025-04-27 00:03:29 +0530

    Add files via upload

---
+++ CommandLineDemo.java
@@ -0,0 +1,7 @@
+public class CommandLineDemo {
+    public static void main(String[] args) {
+        for (String arg : args) {
+            System.out.println("Argument : " + arg);
+        }
+    }
+}
